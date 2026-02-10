a5723dbe3bfacb0f84f280633e940ef30d86a53f
Joshua Nixon <nixonjoshua98@gmail.com>
2026-02-10 19:11:47 +0000

    Initial tests project

---
+++ Source/Portic.Tests/Portic.Transport.RabbitMQ.UnitTests/Helpers/TestMessageConsumer2.cs
@@ -0,0 +1,15 @@
+﻿using Portic.Consumers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portic.Transport.RabbitMQ.UnitTests.Helpers
+{
+    internal sealed class TestMessageConsumer2 : IConsumer<TestMessage>
+    {
+        public ValueTask ConsumeAsync(IConsumerContext<TestMessage> context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
