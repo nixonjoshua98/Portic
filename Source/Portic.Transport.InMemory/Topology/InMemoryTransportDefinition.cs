efad75f000657f0b64d59117fe1dae153f91ad0a
Joshua Nixon <nixonjoshua98@gmail.com>
2026-02-10 19:21:43 +0000

    Misc

--- Source/Portic.Transport.InMemory/Topology/InMemoryTransportDefinition.cs
+++ Source/Portic.Transport.InMemory/Topology/InMemoryTransportDefinition.cs
@@ -1,6 +1,5 @@
 ﻿namespace Portic.Transport.InMemory.Topology
 {
-
     internal sealed class InMemoryTransportDefinition : ITransportDefinition, IInMemoryTransportConfigurator
     {
         public string DisplayName { get; } = "InMemory";
